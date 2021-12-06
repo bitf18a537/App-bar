e94d0a7a9f2392710f97815eeee63515a225f1e8
bitf18a537 <92253656+bitf18a537@users.noreply.github.com>
2021-12-06 21:19:09 +0500

    simple app bar

--- mainactivity.java
+++ mainactivity.java
@@ -17,14 +17,5 @@ protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_my_child);
 
-    // my_child_toolbar is defined in the layout file
-    Toolbar myChildToolbar =
-        (Toolbar) findViewById(R.id.my_child_toolbar);
-    setSupportActionBar(myChildToolbar);
-
-    // Get a support ActionBar corresponding to this toolbar
-    ActionBar ab = getSupportActionBar();
-
-    // Enable the Up button
-    ab.setDisplayHomeAsUpEnabled(true);
+    
 }
